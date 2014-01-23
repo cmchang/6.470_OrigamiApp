7c765044283b6d9a96b5e24872c26a7471dc4b60
cmchang <changc@mit.edu>
2014-01-23 13:41:33 -0500

    Changing dates now functional

--- origami-app/client/views/theFold/theFold.js
+++ origami-app/client/views/theFold/theFold.js
@@ -6,14 +6,21 @@ var changeModal = function(view1, view2){
 weekdays = {0: "Sunday", 1: "Monday", 2: "Tuesday", 3:"Wednesday", 4:"Thursday", 5:"Friday", 6:"Saturday"};
 
 increaseDay = function(day){
-	$(".date_container").removeClass(day);
+	$(".date_container").removeClass(String(day));
 	var next = parseInt(day) + 1;
-	if (next == 8){ next = 0; }
+	if (next == 7){ next = 0; }
 	$(".date_container").addClass(String(next));
 	var day = String(weekdays[next]);
-	$(".date_container").text(day);
+	$(".weekday").text(day);
+};
 
-
+decreaseDay = function(day){
+	$(".date_container").removeClass(String(day));
+	var next = parseInt(day) - 1;
+	if (next == -1){ next = 6; }
+	$(".date_container").addClass(String(next));
+	var day = String(weekdays[next]);
+	$(".weekday").text(day);
 };
 
 Template.theFold.events({
@@ -32,7 +39,6 @@ Template.theFold.events({
 		}else if(modalID === "modal2"){
 			changeModal($(".modal2"), $(".modal3"));
 		}else if(modalID === "modal3"){
-			console.log(currentTrip);
 			Meteor.call("insertTrip",
 				currentTrip.time,
 				currentTrip.type,
@@ -54,7 +60,7 @@ Template.theFold.events({
 
 	},
 	'click .right_arrow' : function(e){
-		if ($(e.target).hasClass("0")){
+		if ($(e.target.parentElement).hasClass("0")){
 			increaseDay(0);
 		}else if($(e.target.parentElement).hasClass("1")){
 			increaseDay(1);
@@ -68,6 +74,24 @@ Template.theFold.events({
 			increaseDay(5);
 		}else if($(e.target.parentElement).hasClass("6")){
 			increaseDay(6);
+		}
+	},
+
+	'click .left_arrow' : function(e){
+		if ($(e.target.parentElement).hasClass("0")){
+			decreaseDay(0);
+		}else if($(e.target.parentElement).hasClass("1")){
+			decreaseDay(1);
+		}else if($(e.target.parentElement).hasClass("2")){
+			decreaseDay(2);
+		}else if($(e.target.parentElement).hasClass("3")){
+			decreaseDay(3);
+		}else if($(e.target.parentElement).hasClass("4")){
+			decreaseDay(4);
+		}else if($(e.target.parentElement).hasClass("5")){
+			decreaseDay(5);
+		}else if($(e.target.parentElement).hasClass("6")){
+			decreaseDay(6);
 		}
 	},
 
