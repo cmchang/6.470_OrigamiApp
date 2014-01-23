91249d04ef18f5da680ef60d60599d9604bec13f
cmchang <changc@mit.edu>
2014-01-23 13:49:16 -0500

    Date js files

---
+++ origami-app/client/views/includes/header.js
@@ -0,0 +1,7 @@
+Template.header.rendered = function(){
+	var weekday = new Date().getDay();
+	var weekdays = {0: "Sunday", 1: "Monday", 2: "Tuesday", 3:"Wednesday", 4:"Thursday", 5:"Friday", 6:"Saturday"};
+	var day = String(weekdays[weekday]);
+	$(".weekday").text(day);
+	$(".date_container").addClass(String([weekday]));
+};
