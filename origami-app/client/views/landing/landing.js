d2b6c72e3f2ec05af62d23a5bcb99e629a59a5d0
Benjamin Chrobot <bchrobot@mit.edu>
2014-01-25 11:41:34 -0500

    More work on landing page

--- origami-app/client/views/landing/landing.js
+++ origami-app/client/views/landing/landing.js
@@ -1,20 +1,36 @@
+var pfoldGeneration = function() {
+	var foldProperties = [
+		{
+			cssVertical: "top",
+			cssHorizontal: "left",
+			folddirection: ["right", "bottom"]
+		}
+	];
+};
+
 Template.landing.rendered = function() {
 
-	var $container = $( '#uc-container' ),
-		pfold = $( '#uc-container' ).pfold({
+	Meteor.setTimeout(function() {
+		// Grab pfold container
+		var $container = $( '.uc-container' );
+		// Instantiate pfold object
+		var pfold = $container.pfold({
 			easing : 'ease-in-out',
-			folds : 3,
-			folddirection : ['left','bottom','right']
+			folds : 2,
+			folddirection : ['right','bottom'],
+			onEndUnfolding : function() {
+				// Fold it back up
+				Meteor.setTimeout(function() {
+					pfold.fold();
+				}, 4*1000);
+			},
+			onEndFolding : function() {
+				// remove it
+				$container.remove();
+			}
 		});
-
-	$container.find( 'span.clickme' ).on( 'click', function() {
-
+		// Unfold automatically
 		pfold.unfold();
-
-	} ).end().find( 'span.close' ).on( 'click', function() {
-
-		pfold.fold();
-
-	} );
+	}, 2*1000);
 	
 };
