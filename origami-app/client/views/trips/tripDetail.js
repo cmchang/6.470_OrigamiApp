ecb9d27400682ab5d17a13e526442b7d928e03c3
Benjamin Chrobot <bchrobot@mit.edu>
2014-01-28 14:54:14 -0500

    Connected markers to list

--- origami-app/client/views/trips/tripDetail.js
+++ origami-app/client/views/trips/tripDetail.js
@@ -1,3 +1,5 @@
+markers = {};
+
 Template.tripDetail.events({
   'click .re-roll-trip': function( e, template ) {
     e.preventDefault();
@@ -13,62 +15,44 @@ Template.tripDetail.events({
 
 });
 
-Template.eventDetail.events({
-  'mouseover .eventDetail': function(e, template){
-    var $controls = $(template.find(".eventIcons")).css('display', 'inline-block');
-  },
-  'mouseout .eventDetail': function(e, template){
-    var $controls = $(template.find(".eventIcons")).css('display', 'none');
-  },
-  'click .glyphicon-remove': function(e, template){
-    var evtDet= $(template.find(".eventDetail"));
-    evtDet.toggle("slide", 500, function(){
-      evtDet.remove();
-      Events.remove(template.data._id);
-    });
-  },
-  'click .glyphicon-repeat': function(e, template){
-    var evtDet= $(e.target.parentNode.parentNode.parentNode.parentNode);
-    evtDet.toggle("slide", 500, function(){
-      evtDet.remove();
-    });
-  }
-
-});
-
 // Track map markers for event locations - TODO
 // Unfold the trip detail view - TODO
 Template.tripDetail.rendered = function() {
-  var markers = {};
   var tO = -1;
   
   Events.find({}).observeChanges({
     added: function( _id, fields ) {
       var loc = [fields.location.latitude, fields.location.longitude];
       markers[_id] = L.marker(loc, {
-        _id: _id
+        opacity: 0.5
       })
       .on('mouseover', function(e){
         $("#li-"+_id).addClass("hovered");
+        e.target.setOpacity(1.0);
       })
       .on('mouseout', function(e){
         $("#li-"+_id).removeClass("hovered");
+        e.target.setOpacity(0.5);
       })
       .addTo(window.markersLayer)
       .bindPopup(fields.name);
 
-      Meteor.clearTimeout(tO);
-      tO = Meteor.setTimeout(function(){
-        var bounds = $.map( markers, function( value, indexOrKey ) {
-          return value.getLatLng();
-        });
-        window.map.fitBounds(bounds, {
-          paddingTopLeft: [20, 80],
-          paddingBottomRight: [400, 20]
-        });
-      }, 1000);
+      // Meteor.clearTimeout(tO);
+      // tO = Meteor.setTimeout(function(){
+      //   var bounds = $.map( markers, function( value, indexOrKey ) {
+      //     return value.getLatLng();
+      //   });
+      //   window.map.fitBounds(bounds, {
+      //     paddingTopLeft: [20, 80],
+      //     paddingBottomRight: [400, 20]
+      //   });
+      // }, 1000);
     },
+    // updated: function( _id, fields ) {
+    //   markers[_id].
+    // },
     removed: function( _id ) {
+      console.log(markers[_id]);
       window.markersLayer.removeLayer(markers[_id]);
     }
   });
@@ -93,4 +77,35 @@ Template.tripDetail.rendered = function() {
 Template.tripDetail.destroyed = function() {
   // catch-all to clear any remaining markers and/or pfold data - TODO
 };
+
+
+
+Template.eventDetail.events({
+  'mouseover .eventDetail': function(e, template){
+    var id = template.data._id;
+    var marker = markers[id];
+    marker.setOpacity(1.0);
+    var $controls = $(template.find(".eventIcons")).css('display', 'inline-block');
+  },
+  'mouseout .eventDetail': function(e, template){
+    var id = template.data._id;
+    var marker = markers[id];
+    marker.setOpacity(0.5);
+    var $controls = $(template.find(".eventIcons")).css('display', 'none');
+  },
+  'click .glyphicon-remove': function(e, template){
+    var evtDet= $(template.find(".eventDetail"));
+    evtDet.toggle("slide", 500, function(){
+      evtDet.remove();
+      Events.remove(template.data._id);
+    });
+  },
+  'click .glyphicon-repeat': function(e, template){
+    var evtDet= $(e.target.parentNode.parentNode.parentNode.parentNode);
+    evtDet.toggle("slide", 500, function(){
+      evtDet.remove();
+    });
+  }
+
+});
 
