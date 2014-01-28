12b1616a4f9f16f5afd6bc92d59164011f8a40f0
cmchang <changc@mit.edu>
2014-01-28 14:46:37 -0500

    my trips css

--- origami-app/client/views/myTrips/myTrips.js
+++ origami-app/client/views/myTrips/myTrips.js
@@ -3,3 +3,22 @@ Template.myTrips.helpers({
     return Trips.find({}).count() > 0;
   }
 });
+
+Template.myTrips.events({
+	'mouseover .tripContainer': function(e) {
+		var tripDetail = e.target;
+		while (tripDetail.className != "tripContainer"){
+			tripDetail = tripDetail.parentNode;
+		}
+		$(tripDetail.childNodes[1]).css("font-weight","600")
+		$(tripDetail.childNodes[3]).show();
+	},
+	'mouseout .tripContainer': function(e) {
+		var tripDetail = e.target;
+		while(tripDetail.className != "tripContainer"){
+			tripDetail = tripDetail.parentNode;
+		}
+		$(tripDetail.childNodes[1]).css("font-weight","300")
+		$(tripDetail.childNodes[3]).hide();
+	}
+});
