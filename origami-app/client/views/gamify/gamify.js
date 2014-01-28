83d32551716ed2574beeee16b7bee8fcbba2e302
cmchang <changc@mit.edu>
2014-01-28 02:05:08 -0500

    badges

--- origami-app/client/views/gamify/gamify.js
+++ origami-app/client/views/gamify/gamify.js
@@ -4,12 +4,16 @@ Template.gamify.helpers({
   }
 });
 
+tempOpacity = "";
 Template.gamify.events({
 	'mouseover .badge': function( e, template ) {
 		$(e.target.parentNode.childNodes[3]).show();
+		tempOpacity = $(e.target.parentNode.childNodes[1]).css("opacity");
+		$(e.target.parentNode.childNodes[1]).css("opacity", "1");
 	},
 	'mouseout .badge':function(e, templage){
 		$(e.target.parentNode.childNodes[3]).hide();
+		$(e.target.parentNode.childNodes[1]).css("opacity", tempOpacity);
 	}
 
 });
